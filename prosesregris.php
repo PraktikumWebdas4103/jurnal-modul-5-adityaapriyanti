6813d5045c4bd06a47f68371739b5af438588112
adityaapriyanti <43569097+adityaapriyanti@users.noreply.github.com>
2018-10-02 16:52:49 +0700

    Update prosesregris.php

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,22 @@
 <?php
-
-
-?>
+ $hostname   = "localhost";
+ $username = "root";
+ $pass     = "";
+ $db       = "regist";
+ $con     = new mysqli($hostname, $username, $pass, $db);
+ if($con==false){
+  die("koneksi gagal". $con->connect_error);
+ }
+  if (isset($_POST['submit'])) {
+   $nim=$_POST['nim'];
+   $nama=$_POST['nama'];
+   $email=$_POST['email'];
+ 
+ $sql="INSERT INTO data(nim,nama,email) VALUES ('$nim','$nama','$email')";
+   if(mysqli_query($con,$sql)){
+    echo "Database sudah masuk";
+   }else{
+    echo "error";
+   }
+}
+ ?>
